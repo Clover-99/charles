c4a553a7b2cc7e8deb77df9cd6c15cfdb4fb76b8
unknown <charlesmadronero@gmail.com>
2024-12-03 15:36:53 +0800

    charles

---
+++ home.php
@@ -0,0 +1,38 @@
+  <section id="banner">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3></h3> 
+          <p></p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3></h3> 
+          <p></p> 
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+ 
+  <?php
+   { 
+      include("category.php");
+    }
+  ?>
+                       
+             
+            <br>
+           
+            </div>
+            
+          </div>
